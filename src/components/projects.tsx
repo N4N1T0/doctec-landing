8fd073a203f96ea19c6d9b5246e583d94e6a5dc3
N4N1T0 <68034867+N4N1T0@users.noreply.github.com>
2024-02-07 18:53:01 +0100

    refactor: new animations

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -13,10 +13,10 @@ const Projects = () => {
           projects.map(project => (
             <a href={project.link} target='_blank' key={project.label} className='relative col-span-1 space-y-1 md:space-y-3 group' aria-label={project.label}>
               <div className='overflow-clip '>
-                <Image src={project.image} alt={project.label} title={project.description} className='group-hover:scale-110 transition-transform duration-200' />
+                <Image src={project.image} alt={project.label} title={project.description} className='group-hover:scale-110 transition-transform group-hover:duration-700 duration-200' />
               </div>
-              <h4 className='text-3xl font-bold group-hover:text-primary transition-colors duration-200'>{project.label}</h4>
-              <p className='group-hover:text-secondary transition-colors duration-200 font-light'>{project.description}</p>
+              <h4 className='text-3xl font-bold group-hover:text-primary transition-colors group-hover:duration-700 duration-200'>{project.label}</h4>
+              <p className='group-hover:text-secondary transition-colors group-hover:duration-700 duration-200 font-light'>{project.description}</p>
             </a>
           ))
         }
